7a6c685d58525673adbf6bc8f0ff36336ae4c6d9
Samuel To <samuelto888@yahoo.co.uk>
2023-11-18 20:36:51 +0000

    add subtract feature

--- app/app/calc.py
+++ app/app/calc.py
@@ -11,3 +11,8 @@ def add(x, y):
 def mult(x, y):
 
     return x * y
+
+
+def subtract(x, y):
+
+    return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -9,7 +9,13 @@ from app import calc
 class CalcTests(SimpleTestCase):
     """test the calc module"""
 
-    def test_add_numbert(self):
+    def test_add_numbers(self):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
+
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, -5)
