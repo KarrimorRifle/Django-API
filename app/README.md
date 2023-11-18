47f3dc7b9df2c877342776d07a22ed4c689163f2
Samuel To <samuelto888@yahoo.co.uk>
2023-11-18 20:16:56 +0000

    Create test file

--- app/README.md
+++ app/README.md
@@ -4,3 +4,11 @@
 
 ## Connectin
 use localhost:8000
+
+## tests
+### Running
+python manage.py test
+
+### Creating tests
+In the app add 'tests.py' file
+now just copy the setup.
---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """test the calc module"""
+
+    def test_add_numbert(self):
+        res = calc.add(5,6)
+
+        self.assertEqual(res,11)
